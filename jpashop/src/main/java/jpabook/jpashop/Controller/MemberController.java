4518e0b8d9bb04415a28aa7957850e88b5ad80e1
twkim5235 <sssunggure@naver.com>
2022-03-21 22:59:23 +0900

    [ADD] 회원 조회 API 개발

--- jpashop/src/main/java/jpabook/jpashop/Controller/MemberController.java
+++ jpashop/src/main/java/jpabook/jpashop/Controller/MemberController.java
@@ -44,7 +44,7 @@ public class MemberController {
 
     @GetMapping
     public String list(Model model) {
-        List<Member> members = memberService.findMember();
+        List<Member> members = memberService.findMembers();
         model.addAttribute("members", members);
         return "members/memberList";
     }
--- jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -38,7 +38,7 @@ public class MemberService {
      * 회원 전체 조회
      * @return
      */
-    public List<Member> findMember() {
+    public List<Member> findMembers() {
         return memberRepository.findAll();
     }
 
